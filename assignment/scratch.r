6bbc0afd23c954f1662feceae2ac318568ded6c4
antnp <antnp4@gmail.com>
2015-01-14 20:03:58 -0600

    scratch reference

---
+++ assignment/scratch.r
@@ -0,0 +1,24 @@
+#scratch
+
+# wk2
+# for loops
+x = c("a", "b", "c")
+for(i in seq_along(x)) { 
+  print(x[i])
+}
+
+for(i in 1:4) {
+  print(i)
+}
+
+# while
+count = 0
+while(count < 10) {
+    print(count)
+    count = count + 1
+}
+
+for(x in 1:10) {
+    
+}
+
